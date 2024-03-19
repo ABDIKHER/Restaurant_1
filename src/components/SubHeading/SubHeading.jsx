abbd71bea944f9abc652642098a5043e49240180
ABDIKHER <ABDIKHER@gmail.com>
2024-03-19 07:23:57 +0300

    first commit

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {images} from '../../constants'
 
 const SubHeading = ({title}) => (
-  <div style={{marginBottom:'1rem'}}>
+  <div style={{margin:'1.5rem 0 '}}>
     <p className="p__cormorant">{title}</p>
     <img src={images.spoon} alt="spoon" className='spoon__img' />
   </div>
